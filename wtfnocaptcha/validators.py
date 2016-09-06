1edcb1149b255d245ff21d10947c65b07bce5766
Ara Hayrabedian <ara.hayrabedian@gmail.com>
2016-09-06 18:20:35 +0300

    update error codes to correspond with nocaptcha docs. remove now-impossible error flow

--- wtfnocaptcha/validators.py
+++ wtfnocaptcha/validators.py
@@ -21,13 +21,11 @@ class NoCaptcha(object):
 
     # Mapping of response error codes
     errors = {
-        'invalid-site-public-key': u'Invalid public key',
-        'invalid-site-private-key': u'Invalid private key',
-        'invalid-request-cookie': u'Challenge is incorrect',
-        'incorrect-captcha-sol': u'Incorrect captcha solution',
-        'verify-params-incorrect': u'Incorrect parameters',
-        'invalid-referrer': u'Incorrect domain',
-        'recaptcha-not-reachable': u'Could not connect to reCaptcha'
+        'missing-input-secret': 'The secret parameter is missing.',
+        'invalid-input-secret':	'The secret parameter is invalid or malformed.',
+        'missing-input-response': 'The response parameter is missing.',
+        'invalid-input-response': 'The response parameter is invalid or malformed.',
+        'nocaptcha-not-reachable': 'Could not connect to nocaptcha api',
     }
 
     empty_error_text = u'This field is required'
@@ -48,7 +46,7 @@ class NoCaptcha(object):
             response.close()
         except Exception as e:
             logger.error(str(e))
-            raise ValidationError(self.errors['recaptcha-not-reachable'])
+            raise ValidationError(self.errors['nocaptcha-not-reachable'])
 
         return data
 
@@ -68,11 +66,11 @@ class NoCaptcha(object):
         if not data['success']:
             # Show only incorrect solution to the user else show default message
             error_list = data['error-codes']
-            if 'incorrect-captcha-sol' in error_list:
-                raise ValidationError(field.gettext('incorrect-captcha-sol'))
-            else:
-                # Log error message in case it wasn't triggered by user
-                logger.error('nocaptcha response errors: %s' % str(error_list))
-                # put together a string of errors
-                error_text = "\n".join([field.gettext(error_code) for error_code in error_list])
-                raise ValidationError(field.gettext(error_text))
+
+            # Log error message in case it wasn't triggered by user
+            logger.error('nocaptcha response errors: %s' % str(error_list))
+
+            # put together a string of errors
+            error_text = "\n".join([field.gettext(error_code) for error_code in error_list])
+
+            raise ValidationError(field.gettext(error_text))
